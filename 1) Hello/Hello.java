392076e34acc149506869d8611a955f9fa0a7bad
Abhishek Kumar <kumar699.abhishek@gmail.com>
2024-08-25 23:29:49 +0530

    Work with int variables

--- 1) Hello/Hello.java
+++ 1) Hello/Hello.java
@@ -1,5 +1,15 @@
 class Hello {
     public static void main(String a[]) {
-        System.out.print("Hello boss");
+        // System.out.println("Hello boss");
+        // System.out.print(3+5);
+        // System.out.println(3+5);
+
+        int num1 = 3;
+        int num2 = 5;
+        System.out.println(num1 +num2 );
     }
 }
+// 1. Compile: javac Hello.javac
+// 2. Run: java Hello
+
+// Dont put semicolon at end of block
