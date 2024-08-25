69711bdaeb3f056e0796c9224f6c134373807a06
Abhishek Kumar <kumar699.abhishek@gmail.com>
2024-08-26 00:54:06 +0530

    Work with threads

---
+++ Threads/Demo.java
@@ -0,0 +1,24 @@
+class A {
+    public void show () {
+        for (int i = 0; i<10; i++) {
+
+            System.out.println("Hi A");
+        }
+    }
+}
+class B {
+    public void show () {
+        for (int i = 0; i<10; ++i) {
+            System.out.println("Hi B");
+        }
+    }
+}
+
+public class Demo {
+    public static void main(String args[]) {
+        A a = new A();
+        B b = new B();
+        a.show();
+        b.show();
+    }
+}
