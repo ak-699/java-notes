c538efbefb597b0458abfd9e490eb6b0602845e5
Abhishek Kumar <kumar699.abhishek@gmail.com>
2024-08-25 23:53:56 +0530

    Work with literals

--- 2) Data Types/Data.java
+++ 2) Data Types/Data.java
@@ -5,7 +5,8 @@ public class Data {
         // byte b = 129 // error cannnot convert int to byte 
         byte b = 127;
         short sh = 558;
-        // literals 
+
+        // Integer literals 
         int num1 = 0b1101;
         int num2 = 0x7E;
         int num3 = 1_00_00_000;
@@ -13,6 +14,20 @@ public class Data {
         // System.out.println(num1); // prints 13
         // System.out.println(num2); // prints 126
         // System.out.println(num3); // prints 10000000
+
+        // Floating literals
+        double num4 = 12e10;
+        // System.out.println(num4); // prints 1.2E11
+
+        // boolean literals
+        // boolean bln = 1; // error: cannot convert int to boolean
+
+        // char literals 
+        char c = 'a';
+        c++;
+        System.out.println(c);
+
+
     }
 }
 
